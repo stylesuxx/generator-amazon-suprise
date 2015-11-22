65b3788378c817fe53c1211cf14b3d257f82ee71
stylesuxx <stylesuxx@gmail.com>
2015-11-22 23:26:42 +0100

    Moved prompts to a seperate file

--- app/index.js
+++ app/index.js
@@ -1,4 +1,7 @@
+'use strict';
 var generators = require('yeoman-generator');
+var chalk = require('chalk');
+var AWL = require('amazon-wish-list');
 
 module.exports = generators.Base.extend({
   helper: {
@@ -12,23 +15,83 @@ module.exports = generators.Base.extend({
   initializing: function() {
     this.author = { name: 'Chris Landa', email: 'stylesuxx@gmail.com'};
     this.appname = this.helper.cleanAppname(this.appname);
+    this.lists = [];
+    this.cidValid = false;
+
+    this.prompts = require('./prompts');
+
+    this.baseConfig = () => {
+      return this.prompts.getAppname(this).then((appname) => {
+        this.appname = appname;
+
+        return this.prompts.getTld(this);
+      }).then((tld) => {
+        this.tld = tld;
+
+        return new Promise((resolve, reject) => resolve());
+      });
+    };
+
+    this.cidConfig = () => {
+      return this.prompts.getCid(this).then((cid) =>{
+        this.cid = cid;
+
+        this.log(chalk.green('>'), chalk.bold('Fetching public lists...'));
+        var awl = new AWL(this.tld);
+        return awl.getByCid(this.cid);
+      }).then((lists) => {
+        this.cidValid = true;
+        var choices = [];
+        for(let list of lists) {
+          choices.push({
+            name: list.title + ' (' + list.items.length + ')',
+            value: list.id
+          })
+        }
+
+        if(choices.length > 0) {
+          return this.prompts.getEnabledLists(this, choices);
+        }
+        else {
+          this.log(chalk.orange('!'), chalk.bold('No public wish lists for this customer ID.'));
+          return new Promise((resolve, reject) => resolve([]));
+        }
+      }, (error) => {
+        this.log(chalk.red('!'), chalk.bold('Customer ID is not valid - can not get wish lists!'));
+        return this.prompts.getRetry(this).then((retry) => {
+          if(retry) {
+            return this.cidConfig();
+          }
+          else {
+            return new Promise((resolve, reject) => resolve([]));
+          }
+        });
+      });
+    }
   },
 
-  prompting: {
-    appname: function() {
-      var done = this.async();
+  prompting: function() {
+    var done = this.async();
 
-      this.prompt({
-        type: 'input',
-        name: 'appname',
-        message: 'Generator name',
-        default: this.appname
-      }, function(answers) {
-        this.appname = this.helper.cleanAppname(answers.appname);
+    this.baseConfig().then(() => {
+      return this.cidConfig();
+    }).then((lists) => {
+      this.lists = lists;
 
-        done();
-      }.bind(this));
-    }
+      return this.prompts.getMinLimit(this);
+    }).then((limit) => {
+      this.minLimit = limit;
+
+      return this.prompts.getMaxLimit(this);
+    }).then((limit) => {
+      this.maxLimit = limit;
+
+      return this.prompts.getMultiple(this);
+    }).then((multiple) => {
+      this.multiple = multiple;
+
+      done();
+    });
   },
 
   configuring: {
@@ -38,6 +101,13 @@ module.exports = generators.Base.extend({
   },
 
   writing: {
+    static: function() {
+      this.copy('.gitignore', '.gitignore');
+    },
+
+    templates: function() {
+      this.template('_config.js', 'config.js');
+    }
   },
 
   conflicts: {
---
+++ app/prompts.js
@@ -0,0 +1,108 @@
+module.exports = {
+  getAppname: function(generator) {
+    return new Promise((resolve, reject) => {
+      generator.prompt({
+        type: 'input',
+        name: 'appname',
+        message: 'Bot name',
+        default: generator.appname
+      }, (answers) => {
+        resolve(generator.helper.cleanAppname(answers.appname));
+      });
+    });
+  },
+
+  getTld: function(generator) {
+    return new Promise((resolve, reject) => {
+      generator.prompt({
+        type: 'list',
+        name: 'tld',
+        message: 'Your amazon order tld',
+        choices: ['de', 'co.uk', 'com'],
+        default: 'de'
+      }, (answers) => {
+        resolve(answers.tld);
+      });
+    });
+  },
+
+  getCid: function(generator) {
+    return new Promise((resolve, reject) => {
+      generator.prompt({
+        type: 'input',
+        name: 'cid',
+        message: 'Amazon Customer ID to look up',
+        default: '14 characters'
+      }, (answers) => {
+        resolve(answers.cid);
+      });
+    });
+  },
+
+  getMinLimit: function(generator) {
+    return new Promise((resolve, reject) => {
+      generator.prompt({
+        type: 'input',
+        name: 'minLimit',
+        message: 'Minimum price',
+        default: 0.00,
+        validate: (value) => !isNaN(value) ? true : 'Minimum price has to be a number'
+      }, (answers) => {
+        resolve(answers.minLimit);
+      });
+    });
+  },
+
+  getMaxLimit: function(generator) {
+    return new Promise((resolve, reject) => {
+      generator.prompt({
+        type: 'input',
+        name: 'maxLimit',
+        message: 'Maximum price',
+        default: 20.00,
+        validate: (value) => !isNaN(value) ? true : 'Maximum price has to be a number'
+      }, (answers) => {
+        resolve(answers.maxLimit);
+      });
+    });
+  },
+
+  getMultiple: function(generator) {
+    return new Promise((resolve, reject) => {
+      generator.prompt({
+        type: 'confirm',
+        name: 'multiple',
+        message: 'Buy multiple items until maximum price is reached?',
+        default: false
+      }, (answers) => {
+        resolve(answers.multiple);
+      });
+    });
+  },
+
+  getEnabledLists: function(generator, lists) {
+    return new Promise((resolve, reject) => {
+      generator.prompt({
+        type: 'checkbox',
+        name: 'lists',
+        message: 'Enable wish lists:',
+        choices: lists
+      }, (answers) => {
+        resolve(answers.lists);
+      });
+    });
+  },
+
+  getRetry: function(generator) {
+    return new Promise((resolve, reject) => {
+      generator.prompt({
+        type: 'confirm',
+        name: 'retry',
+        message: 'Try another customer ID?',
+        default: true
+      }, (answers) => {
+        resolve(answers.retry);
+      });
+    });
+  }
+}
