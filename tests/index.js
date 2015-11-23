827a5c4d85f860d8634736fe0ee32f8cf94b8cf0
stylesuxx <stylesuxx@gmail.com>
2015-11-23 19:37:42 +0100

    Tests for filtering wish lists

--- tests/index.js
+++ tests/index.js
@@ -18,29 +18,36 @@ test('Bot: empty constructor', function(t) {
 
   const bot = new Bot();
   const limits = bot.getPriceLimits();
-  t.equals(limits.min, 0, 'min price limit');
-  t.equals(limits.max, 20, 'max price limit');
-  t.equals(bot.getTld(), 'de', 'tld');
-  t.equals(bot.getListIds().length, 0, 'no list ids');
-  t.equals(bot.getMultiple(), false, 'not ordering multiple');
+  t.equals(limits.min, 0, 'get: min price limit');
+  t.equals(limits.max, 20, 'get: max price limit');
+  t.equals(bot.getTld(), 'de', 'get: tld');
+  t.equals(bot.getListIds().length, 0, 'get: no list ids');
+  t.equals(bot.getMultiple(), false, 'get: not ordering multiple');
 });
 
-test('Bot: testdata', function(t) {
-  t.plan(9);
+test('Bot: wish list fetching', function(t) {
+  t.plan(14);
 
   const bot = new Bot(testData);
   const limits = bot.getPriceLimits();
-  t.equals(limits.min, testData.limits.min, 'min price limit');
-  t.equals(limits.max, testData.limits.max, 'max price limit');
-  t.equals(bot.getTld(), testData.tld, 'tld');
-  t.equals(bot.getListIds().length, testData.lists.length, 'list ids');
-  t.equals(bot.getMultiple(), testData.multiple, 'multiple ordering');
+  t.equals(limits.min, testData.limits.min, 'get: min price limit');
+  t.equals(limits.max, testData.limits.max, 'get: max price limit');
+  t.equals(bot.getTld(), testData.tld, 'get: tld');
+  t.equals(bot.getListIds().length, testData.lists.length, 'get: list ids');
+  t.equals(bot.getMultiple(), testData.multiple, 'get: multiple');
 
   bot.loadLists().then((results) => {
     results = bot.getLists();
-    t.equals(results.length, 2, 'wish list content');
-    t.equals(results[1].title, 'testing', 'title ok');
-    t.equals(results[1].id, testData.lists[1], 'ID ok');
-    t.equals(results[1].items.length > 25, true, 'items ok');
+    t.equals(results.length, 2, 'wish list: content');
+    t.equals(results[1].title, 'testing', 'wish list: title');
+    t.equals(results[1].id, testData.lists[1], 'wish list: ID');
+    t.equals(results[1].items.length > 25, true, 'wish list: Items');
+    
+    var filtered = bot.getFiltered();
+    t.equals(filtered.length > 25, true, 'wish list filtered: items');
+    t.ok(filtered[0].id, 'wish list filtered: item id');
+    t.ok(filtered[0].title, 'wish list filtered: item title');
+    t.ok(filtered[0].currency, 'wish list filtered: item currency');
+    t.ok(filtered[0].price <= testData.limits.max && filtered[0].price >= testData.limits.min, 'wish list filtered: item price');
   });
 });
