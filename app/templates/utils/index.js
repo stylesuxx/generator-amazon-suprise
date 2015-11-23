7f0c04690788edb196aaa378c265537643865a47
stylesuxx <stylesuxx@gmail.com>
2015-11-23 01:08:34 +0100

    Use bluebird to resolve dependencies

--- app/templates/utils/index.js
+++ app/templates/utils/index.js
@@ -1,9 +1,36 @@
-'use strict;'
+'use strict';
 const AWL = require('amazon-wish-list');
+const Promise = require('bluebird');
 
 module.exports = {
   getItemsInRange: function(tld, ids, min, max) {
     const awl = new AWL(tld);
+    var promises = [];
+    var items = [];
+
+    for(let id of ids) {
+      promises.push(awl.getById(id));
+    }
+
+    return Promise.all(promises).then((results) => {
+      for(let list of results) {
+        for(let item of list.items) {
+          if(item.price <= max && item.price >= min) {
+            items.push(item);
+          }
+        }
+      }
+
+      return items;
+    });
+  },
+
+  selectItem: function(ids) {
+
+  },
+
+  selectItems: function(ids) {
+
   },
 
   getListsByCid: function(tld, cid) {
